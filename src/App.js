9df4d8bdc39b1218a7c617df9e204ae69972e3d6
said.T <tarhisaid@gmail.com>
2021-06-07 22:13:05 +0100

    React calculator suite
    UseState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+
+import { render } from '@testing-library/react';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
+  
+
+
+
+
+  
+
 function App() {
+
+  const myNumber = [1,2,3,4,5,6,7,8,9,0]
+  const myOperatorList=["-","+","/","X"]
+
+  const [resultat,setMyResult]=useState(
+    {currentValue:"",
+    oldValue:"0",
+    myOperator:""})
+
+ 
+  const myTableOperateur = myOperatorList.map((operator)=>(<button onClick={()=>setMyResult({oldValue:resultat.currentValue,currentValue:""})} key={operator}>{operator}</button>))
+  const myTableNumber= myNumber.map((element)=>(<button onClick={()=>setMyResult({currentValue:resultat.currentValue+element})} key={element.toString()}>{element}</button>));
+  const myEqual = <button id="Equal" onClick={()=>setMyResult({currentValue:typeof(resultat.currentValue)})}>=</button>
+  
+  
+
+
+
+
+
+  
+  
+
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="myCalculator">
+          <span id="Resultat" children={resultat.currentValue} ></span>
+          {myEqual}
+          {myTableNumber}
+          {myTableOperateur}
+    </div>
       </header>
     </div>
   );
