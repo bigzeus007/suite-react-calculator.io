f9b975463d2893ce9109d9beac444861d7d37187
said.T <tarhisaid@gmail.com>
2021-06-07 23:56:19 +0100

    React UseState
    Training courses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Number } from '@svgdotjs/svg.js';
 import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import './App.css';
@@ -17,15 +18,15 @@ function App() {
   const myNumber = [1,2,3,4,5,6,7,8,9,0]
   const myOperatorList=["-","+","/","X"]
 
-  const [resultat,setMyResult]=useState(
-    {currentValue:"",
-    oldValue:"0",
+  const [resultat,setMyResult]=useState({
+    Affichage:0,
+    currentValue:0,
+    oldValue:0,
     myOperator:""})
-
  
-  const myTableOperateur = myOperatorList.map((operator)=>(<button onClick={()=>setMyResult({oldValue:resultat.currentValue,currentValue:""})} key={operator}>{operator}</button>))
-  const myTableNumber= myNumber.map((element)=>(<button onClick={()=>setMyResult({currentValue:resultat.currentValue+element})} key={element.toString()}>{element}</button>));
-  const myEqual = <button id="Equal" onClick={()=>setMyResult({currentValue:typeof(resultat.currentValue)})}>=</button>
+  const myTableOperateur = myOperatorList.map((operator)=>(<button onClick={()=>setMyResult({...resultat,myOperator:operator,oldValue:resultat.currentValue,currentValue:""})} key={operator}>{operator}</button>))
+  const myTableNumber= myNumber.map((element)=>(<button onClick={()=>setMyResult({...resultat,currentValue:(resultat.currentValue+element),Affichage:(resultat.currentValue+element)})} key={element}>{element}</button>))
+  const myEqual = <button id="Equal" onClick={()=>setMyResult({...resultat,Affichage:resultat.oldValue+resultat.currentValue})}>=</button>
   
   
 
@@ -42,7 +43,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div id="myCalculator">
-          <span id="Resultat" children={resultat.currentValue} ></span>
+          <span id="Resultat" children={resultat.Affichage} ></span>
           {myEqual}
           {myTableNumber}
           {myTableOperateur}
